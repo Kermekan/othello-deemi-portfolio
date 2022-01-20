1f8dd650ee9f456572d45df8c7d914adebace6e3
Kermekan <odeemijr@gmail.com>
2022-01-20 03:49:33 +0000

    added slider for portfolio and made it functional

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,14 +19,20 @@ menuBtn.addEventListener("click", () => {
 });
 
 // Typewriter =========
+
+// Variable to display messages in the DOM
 const textDisplay = document.getElementById("typewriter");
+// The array that holds the phrases
 const phrases = [
     `and I'm a Web Developer `,
      `I love Coding `,
 ];
 
-let i = 0;
+// Out loop iterator
+let i = 0; 
+// Inner loop iterator
 let j = 0;
+// Holds each letter of current phrase
 let typewriter = [];
 let isDeleting = false
 isEnd = false;
@@ -112,6 +118,7 @@ inputs.forEach(input => {
 });
 
 // Form Validation =========
+// Variables for my form
 const contactForm = document.querySelector("#contactForm");
 const firstName = document.querySelector("#first-name");
 const lastName = document.querySelector("#last-name");
@@ -205,9 +212,99 @@ contactForm.addEventListener( "submit", (e) => {
         })
 });
 
+// Carousel =========
+const slider = document.querySelector(".portfolio-carousel")
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".portfolio-slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+// Slide image on nextBtn click
+nextBtn.addEventListener("click", () => {
+    // Remove active class from previous slide
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    });
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active")
+    });
+    
+    // increment iterator
+    slideNumber++;
+    
+    // go to next slide
+    if (slideNumber > (numberOfSlides - 1)) {
+        slideNumber = 0;
+    }
+    
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+});
+
+// Slide image on prevBtn click
+prevBtn.addEventListener("click", () => {
+    // Remove active class from previous slide
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    });
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active")
+    });
+    
+    // increment iterator
+    slideNumber--;
+    
+    if (slideNumber < 0) {
+        slideNumber = numberOfSlides - 1;
+    }
+    
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+}); 
+
+// Autoplay image slider
+let playSlider;
+
+let repeater = () => {
+    playSlider = setInterval(function () {
+        // Remove active class from previous slide
+        slides.forEach((slide) => {
+            slide.classList.remove("active")
+        });
+        slideIcons.forEach((slideIcon) => {
+            slideIcon.classList.remove("active")
+        });
+    
+        // increment iterator
+        slideNumber++;
+    
+        if (slideNumber > (numberOfSlides - 1)) {
+            slideNumber = 0;
+        }
+    
+        slides[slideNumber].classList.add("active");
+        slideIcons[slideNumber].classList.add("active");
+    },4000);
+}
+
+repeater();
+
+// Stop autoplay on mouseover
+slider.addEventListener("mouseover", () => (
+    clearInterval(playSlider)
+));
+
+// start autoplay on mouseout
+slider.addEventListener("mouseout", () => (
+    repeater()
+));
+    
 
 
-// console.log(errors);
+
+// console.log(numberOfSlides);
 
 
 
