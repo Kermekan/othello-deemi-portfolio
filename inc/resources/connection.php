547614bf8a17505b8bd6ede0a0605c3c7b20985a
Kermekan <odeemijr@gmail.com>
2022-04-27 08:34:37 +0100

    added cpanel database details

--- inc/resources/connection.php
+++ inc/resources/connection.php
@@ -1,8 +1,16 @@
-<?php 
-$dbhost = "localhost";
+<?php
+
+// Local
+/* $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "portfolio";
+$dbname = "portfolio"; */
+
+// cPanel
+$dbhost = "localhost";
+$dbuser = "othellod_othellodeemi";
+$dbpass = "270301Lonestar1!";
+$dbname = "othellod_portfolio";
 
 $conn =  mysqli_connect($dbhost, $dbuser, $dbpass, $dbname,);
 
