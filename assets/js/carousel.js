58ec43e5dc6b19ca8e6624a9a309b70a913f9b06
Kermekan <odeemijr@gmail.com>
2022-06-24 14:51:59 +0100

    added portfolio carousel for mobile view. I also created seperate functions for both viewport widths.

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,89 +1,27 @@
 /* -------------------------------- Carousel -------------------------------- */
-const slider = document.querySelector(".portfolio-carousel")
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
-const slides = document.querySelectorAll(".portfolio-slide");
-const slideIcons = document.querySelectorAll(".slide-icon");
-const numberOfSlides = slides.length;
-let slideNumber = 0;
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    enabled: true,
+    loop: true,
+    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
-// Slide on nextBtn click
-nextBtn.addEventListener("click", () => {
-    // Remove active class from previous slide
-    slides.forEach((slide) => {
-        slide.classList.remove("active")
-    });
-    slideIcons.forEach((slideIcon) => {
-        slideIcon.classList.remove("active")
-    });
-    
-    // increment iterator
-    slideNumber++;
-    
-    // go to next slide
-    if (slideNumber > (numberOfSlides - 1)) {
-        slideNumber = 0;
-    }
-    
-    slides[slideNumber].classList.add("active");
-    slideIcons[slideNumber].classList.add("active");
-});
-
-// Slide on prevBtn click
-prevBtn.addEventListener("click", () => {
-    // Remove active class from previous slide
-    slides.forEach((slide) => {
-        slide.classList.remove("active")
-    });
-    slideIcons.forEach((slideIcon) => {
-        slideIcon.classList.remove("active")
-    });
-    
-    // increment iterator
-    slideNumber--;
-    
-    if (slideNumber < 0) {
-        slideNumber = numberOfSlides - 1;
-    }
-    
-    slides[slideNumber].classList.add("active");
-    slideIcons[slideNumber].classList.add("active");
-}); 
-
-// Autoplay slider
-let playSlider;
-
-let repeater = () => {
-    playSlider = setInterval(function () {
-        // Remove active class from previous slide
-        slides.forEach((slide) => {
-            slide.classList.remove("active")
-        });
-        slideIcons.forEach((slideIcon) => {
-            slideIcon.classList.remove("active")
-        });
-    
-        // increment iterator
-        slideNumber++;
-    
-        if (slideNumber > (numberOfSlides - 1)) {
-            slideNumber = 0;
-        }
-    
-        slides[slideNumber].classList.add("active");
-        slideIcons[slideNumber].classList.add("active");
-    },4000);
-}
-
-repeater();
-
-// Stop autoplay on mouseover
-slider.addEventListener("mouseover", () => (
-    clearInterval(playSlider)
-));
-
-// start autoplay on mouseout
-slider.addEventListener("mouseout", () => (
-    repeater()
-));
-
+console.log(menuBtn);
+console.log(swiper);
--- inc/layout/portfolio.php
+++ inc/layout/portfolio.php
@@ -1,9 +1,7 @@
 <?php 
-include ('inc/resources/connection.php'); 
+include ('inc/resources/portfolio-query.php'); 
 
-// Projects Query
-$projectsQuery = "SELECT * FROM projects ORDER BY id ASC";
-$projectsResult = mysqli_query($conn, $projectsQuery);
+
 ?>
 <!-- ------------------------------ Portfolio ------------------------------- -->
 <section id="portfolio" class="portfolio">
@@ -16,129 +14,34 @@ $projectsResult = mysqli_query($conn, $projectsQuery);
                 <p class="lead pt-3">A Showing of my work</p>
             </div>
             <!-- Desktop Cards -->
-            <div class="portfolio-box portfolio-tilt grid hidden-xs hidden-sm hidden-md hidden-lg">
-                               
+            <div class="portfolio-box grid hidden-xs hidden-sm hidden-md hidden-lg">
                 <?php 
-                // include('inc/resources/portfolio-query.php');
-                
-                try {
-                    while($projects = mysqli_fetch_assoc($projectsResult)) {
-                        $description = htmlspecialchars($projects["description"]);
-                        $title = htmlspecialchars($projects["title"]);
-                        $link = htmlspecialchars($projects["link"]);
-                        $img_src = htmlspecialchars($projects["img_src"]);
-                        
-                        echo ("
-                        <div class='cards portflio-card flex'>
-                            <!-- Image -->
-                            <div class='img-container img-fluid'>
-                                <img src='$img_src' alt='$title'>
-                            </div>
-                            <!-- Details-->
-                            <div class='details'>
-                                <div class='details-inner flex'>
-                                    <h3 class='title text-capitalize'>$title</h3>
-                                    <p class=''>
-                                        $description
-                                    </p>
-                                    <!-- Button -->
-                                    <div class='btn portfolio-btn'>
-                                        <a href='$link' target='_blank' class='text-center flex text-uppercase'>
-                                            Read More
-                                        </a>
-                                    </div>
-                                </div>
-                            </div><!-- .details -->
-                        </div><!-- .portfolio-card -->
-                        ");
-                    }
-                } catch (\Exception $e) {
-                    echo "<p>Error: " . $e->getMessage() . "</p>";
-                }
-                
+                projectsDesktop();
                 ?>
             </div><!-- .portfolio-box -->
             
             <!-- Mobile/Tablet Cards -->
             <div class="portfolio-box grid portfolio-carousel hidden-xl hidden-xxl">
-                <!-- Card 1 -->
-                <div class="cards portfolio-slide flex active">
-                    <!-- Image -->
-                    <div class="img-container img-fluid">
-                        <img src="assets/images/portfolio/netmatters-home.png" alt="Netmatters Home Page">
+                <div class="swiper">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <?php 
+                        projectsMobile();
+                        ?>
+                        
                     </div>
-                    <!-- Details -->
-                    <div class="details">
-                        <div class="details-inner flex">
-                            <h3 class="title">Netmatters Homepage</h3>
-                            <p class="">
-                                I rebuilt the Netmatters homepage using the skills I learnt in this training course
-                            </p>
-                            <!-- Button -->
-                            <div class="btn portfolio-btn">
-                                <a href="netmatters.othello.deemi.netmatters-scs.co.uk/" target="_blank" class="text-center flex text-uppercase">
-                                    Read More
-                                </a>
-                            </div>
-                        </div>
+                    <!-- If we need pagination -->
+                    <div class="swiper-pagination"></div>
+
+                    <!-- If we need navigation buttons -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+
+                    <!-- If we need scrollbar -->
+                    <!-- <div class="swiper-scrollbar"></div> -->
                     </div>
-                </div><!-- .portfolio-card -->
-                <!-- Card 2 -->
-                <div class="cards portfolio-slide flex">
-                    <!-- Image -->
-                    <div class="img-container img-fluid">
-                        <img src="assets/images/portfolio/arrays-reflection.png" alt="JavaScript Arrays Reflection">
-                    </div>
-                    <!-- Details -->
-                    <div class="details">
-                        <div class="details-inner flex">
-                            <h3 class="title">Random Image Generator </h3>
-                            <p class="">
-                                I worte a random image generator using JavaScript Async Await to fetch images from the PicSum API and save them to a validated email address.
-                            </p>
-                            <!-- Button -->
-                            <div class="btn portfolio-btn">
-                                <a href="js-arrays.othello.deemi.netmatters-scs.co.uk/" class="text-center flex text-uppercase" target="_blank">
-                                    Read More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- .portfolio-card -->
-                <!-- Card 3 -->
-                <div class="cards portfolio-slide flex">
-                    <!-- Image -->
-                    <div class="img-container img-fluid">
-                        <img src="assets/images/portfolio/portfolio-img.jpg" alt="Netmatters Home Page">
-                    </div>
-                    <!-- Details -->
-                    <div class="details">
-                        <div class="details-inner flex">
-                            <h3 class="title">Portfolio 2</h3>
-                            <p class="">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, illum?
-                            </p>
-                            <!-- Button -->
-                            <div class="btn portfolio-btn">
-                                <a href="#" class="text-center flex text-uppercase">
-                                    Read More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- .portfolio-card -->
-                <!-- Navigation buttons -->
-                <div class="navigation flex">
-                    <i class="fas fa-chevron-left prev-btn"></i>
-                    <i class="fas fa-chevron-right next-btn"></i>
-                </div>
-                <!-- owl dots -->
-                <div class="navigation-visibility flex">
-                    <div class="slide-icon active"></div>
-                    <div class="slide-icon"></div>
-                    <div class="slide-icon"></div>
-                </div>
-            </div><!-- .portfolio-box --> 
+            </div><!-- .portfolio-box -->
         </div><!-- .content --> 
     </div><!-- .portfolio-inner -->
 </section>
