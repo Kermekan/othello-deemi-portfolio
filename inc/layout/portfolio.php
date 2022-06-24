a7df1f86ee39fc54ab32c3bc1799bea16c23d4b2
Kermekan <odeemijr@gmail.com>
2022-06-24 09:09:18 +0100

    Added scss files because I accidentally deleted them

--- inc/layout/portfolio.php
+++ inc/layout/portfolio.php
@@ -1,6 +1,7 @@
 <?php 
 include ('inc/resources/connection.php'); 
 
+// Projects Query
 $projectsQuery = "SELECT * FROM projects ORDER BY id ASC";
 $projectsResult = mysqli_query($conn, $projectsQuery);
 ?>
@@ -59,21 +60,21 @@ $projectsResult = mysqli_query($conn, $projectsQuery);
             </div><!-- .portfolio-box -->
             
             <!-- Mobile/Tablet Cards -->
-            <!-- <div class="portfolio-box grid portfolio-carousel hidden-xl hidden-xxl">
-                <!~~ Card 1 ~~>
+            <div class="portfolio-box grid portfolio-carousel hidden-xl hidden-xxl">
+                <!-- Card 1 -->
                 <div class="cards portfolio-slide flex active">
-                    <!~~ Image ~~>
+                    <!-- Image -->
                     <div class="img-container img-fluid">
                         <img src="assets/images/portfolio/netmatters-home.png" alt="Netmatters Home Page">
                     </div>
-                    <!~~ Details ~~>
+                    <!-- Details -->
                     <div class="details">
                         <div class="details-inner flex">
                             <h3 class="title">Netmatters Homepage</h3>
                             <p class="">
                                 I rebuilt the Netmatters homepage using the skills I learnt in this training course
                             </p>
-                            <!~~ Button ~~>
+                            <!-- Button -->
                             <div class="btn portfolio-btn">
                                 <a href="netmatters.othello.deemi.netmatters-scs.co.uk/" target="_blank" class="text-center flex text-uppercase">
                                     Read More
@@ -81,21 +82,21 @@ $projectsResult = mysqli_query($conn, $projectsQuery);
                             </div>
                         </div>
                     </div>
-                </div><!~~ .portfolio-card ~~>
-                <!~~ Card 2 ~~>
+                </div><!-- .portfolio-card -->
+                <!-- Card 2 -->
                 <div class="cards portfolio-slide flex">
-                    <!~~ Image ~~>
+                    <!-- Image -->
                     <div class="img-container img-fluid">
                         <img src="assets/images/portfolio/arrays-reflection.png" alt="JavaScript Arrays Reflection">
                     </div>
-                    <!~~ Details ~~>
+                    <!-- Details -->
                     <div class="details">
                         <div class="details-inner flex">
                             <h3 class="title">Random Image Generator </h3>
                             <p class="">
                                 I worte a random image generator using JavaScript Async Await to fetch images from the PicSum API and save them to a validated email address.
                             </p>
-                            <!~~ Button ~~>
+                            <!-- Button -->
                             <div class="btn portfolio-btn">
                                 <a href="js-arrays.othello.deemi.netmatters-scs.co.uk/" class="text-center flex text-uppercase" target="_blank">
                                     Read More
@@ -103,21 +104,21 @@ $projectsResult = mysqli_query($conn, $projectsQuery);
                             </div>
                         </div>
                     </div>
-                </div><!~~ .portfolio-card ~~>
-                <!~~ Card 3 ~~>
+                </div><!-- .portfolio-card -->
+                <!-- Card 3 -->
                 <div class="cards portfolio-slide flex">
-                    <!~~ Image ~~>
+                    <!-- Image -->
                     <div class="img-container img-fluid">
                         <img src="assets/images/portfolio/portfolio-img.jpg" alt="Netmatters Home Page">
                     </div>
-                    <!~~ Details ~~>
+                    <!-- Details -->
                     <div class="details">
                         <div class="details-inner flex">
                             <h3 class="title">Portfolio 2</h3>
                             <p class="">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, illum?
                             </p>
-                            <!~~ Button ~~>
+                            <!-- Button -->
                             <div class="btn portfolio-btn">
                                 <a href="#" class="text-center flex text-uppercase">
                                     Read More
@@ -125,19 +126,19 @@ $projectsResult = mysqli_query($conn, $projectsQuery);
                             </div>
                         </div>
                     </div>
-                </div><!~~ .portfolio-card ~~>
-                <!~~ Navigation buttons ~~>
+                </div><!-- .portfolio-card -->
+                <!-- Navigation buttons -->
                 <div class="navigation flex">
                     <i class="fas fa-chevron-left prev-btn"></i>
                     <i class="fas fa-chevron-right next-btn"></i>
                 </div>
-                <!~~ owl dots ~~>
+                <!-- owl dots -->
                 <div class="navigation-visibility flex">
                     <div class="slide-icon active"></div>
                     <div class="slide-icon"></div>
                     <div class="slide-icon"></div>
                 </div>
-            </div><!~~ .portfolio-box ~~> -->
-        </div><!-- .content -->
+            </div><!-- .portfolio-box --> 
+        </div><!-- .content --> 
     </div><!-- .portfolio-inner -->
 </section>
